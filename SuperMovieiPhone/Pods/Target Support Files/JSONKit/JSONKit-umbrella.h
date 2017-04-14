33b417537d2b1c58042663f678df945eda8e3119
chenchaozhong <chenchaozhong@chenchaozhongdeMacBook-Pro.local>
2017-04-14 09:00:12 +0800

    delete

--- SuperMovieiPhone/Pods/Target Support Files/JSONKit/JSONKit-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-#import "JSONKit.h"
-
-FOUNDATION_EXPORT double JSONKitVersionNumber;
-FOUNDATION_EXPORT const unsigned char JSONKitVersionString[];
-
--- SuperMovieiPhone/Pods/Target Support Files/Pods-CCZDataModel/Pods-CCZDataModel-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_CCZDataModelVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_CCZDataModelVersionString[];
-
